51004b9c1f2e99d558e2581b6f3c1116d35bb414
deecodez <51487896+deecodez@users.noreply.github.com>
2022-11-16 21:38:11 +0100

    updated firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'presentation/presentation/ui/country_list_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
